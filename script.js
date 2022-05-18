b9a0d98087265cadb0a64a7961f0bac192eee938
Dirty Soap <ytr67c@gmail.com>
2022-05-18 13:28:03 +0300

    bodyacommit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 document.body.innerHTML = '<h1>Hellojs</h1>'
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
